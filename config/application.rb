65a46f550c2539912c5e55acd5d8bd21cc0f1a60
charlesoft <charlees1509@hotmail.com>
2014-06-16 17:57:08 -0300

    modificado arquivo application.rb

--- config/application.rb
+++ config/application.rb
@@ -59,6 +59,7 @@ module ProjectSoftlineinformatica
     # Version of your assets, change this if you want to expire all your assets
     config.assets.version = '1.0'
 
-    config.assets.initialize_on_precompile = false
+    config.assets.precompile += ['*.css', '*.js']
+    
   end
 end
